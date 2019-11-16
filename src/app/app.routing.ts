d6d1cc7be725856bcb0536c1ace2bd3218a79e51
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-16 16:04:55 -0300

    Adds routing for the timeline component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginComponent } from './modules/authentication/login/login.component';
 import { ModuleWithProviders } from '@angular/core';
+import { LoginComponent } from './modules/authentication/login/login.component';
+import { TimelineComponent } from './modules/timeline/timeline.component';
 
 const APP_ROUTES: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'timeline',
+    component: TimelineComponent
   }
 ]
 
