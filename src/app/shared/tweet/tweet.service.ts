8225cbe255beabae2d9353139f20f9606fae05ca
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-17 14:34:41 -0300

    Implements request for tweets from an author

--- src/app/shared/tweet/tweet.service.ts
+++ src/app/shared/tweet/tweet.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Tweet } from 'src/app/modules/timeline/tweet/Tweet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TweetService {
 
-  constructor() { }
+  private readonly API = 'http://localhost:8080/tweets';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTweetsFromAuthor(author: String) {
+    return this.httpClient.get<Tweet[]>(`${this.API}?author=${author}`);
+  }
 }
