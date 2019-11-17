9aded7e1440f46f2ac8515ceb5b2bd39d4a899fe
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-16 23:57:54 -0300

    Removes blank line

--- src/app/modules/timeline/timeline.module.ts
+++ src/app/modules/timeline/timeline.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { TimelineComponent } from './timeline.component';
 import { TweetComponent } from './tweet/tweet.component';
 
