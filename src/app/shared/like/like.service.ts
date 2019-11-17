198e7e7e22e3a46bf353b2c670ef5b3b3769c541
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-17 15:33:47 -0300

    Implements the request for liking a tweet

--- src/app/shared/like/like.service.ts
+++ src/app/shared/like/like.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LikeService {
 
-  constructor() { }
+  private readonly API = 'http://localhost:8080/tweets/{id}/like';
+
+  constructor(private httpClient: HttpClient) { }
+
+  likeTweet(tweetId: string) {
+    const url = this.API.replace('{id}', tweetId);
+
+    return this.httpClient.post(url, {});
+  }
 }
