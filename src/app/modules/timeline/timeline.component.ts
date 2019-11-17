01d6c96bdcd52bc548012f5b7527bc21ac0c46ed
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-17 14:50:12 -0300

    Uses an Observable to get the tweets from the server

--- src/app/modules/timeline/timeline.component.ts
+++ src/app/modules/timeline/timeline.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GeneralService } from 'src/app/shared/general/general-service.service';
-
 import { Tweet } from './tweet/Tweet';
 import { TweetService } from 'src/app/shared/tweet/tweet.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-timeline',
@@ -11,7 +11,7 @@ import { TweetService } from 'src/app/shared/tweet/tweet.service';
 })
 export class TimelineComponent implements OnInit {
 
-  tweets: Tweet[];
+  tweets$: Observable<Tweet[]>;
 
   @ViewChild('newTweet', { static: false }) newTweet: ElementRef;
 
@@ -20,8 +20,7 @@ export class TimelineComponent implements OnInit {
   ngOnInit() {
     const author = localStorage.getItem('username');
 
-    this.tweetService.getTweetsFromAuthor(author)
-    .subscribe(tweets => this.tweets = tweets);
+    this.tweets$ = this.tweetService.getTweetsFromAuthor(author);
   }
 
   handleNewTweet(event) {
