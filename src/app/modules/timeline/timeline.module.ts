a97a33fe94bc615fa0df0b585798ed00b4bee8da
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-16 23:18:31 -0300

    Generates the timeline's module

---
+++ src/app/modules/timeline/timeline.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimelineComponent } from './timeline.component';
+import { TweetComponent } from './tweet/tweet.component';
+
+
+
+@NgModule({
+  declarations: [
+    TimelineComponent,
+    TweetComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TimelineModule { }
