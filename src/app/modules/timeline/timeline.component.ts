0487798e802072f901a59b240cb4cba4976823fb
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-16 23:56:36 -0300

    Handles a new tweet

--- src/app/modules/timeline/timeline.component.ts
+++ src/app/modules/timeline/timeline.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { GeneralService } from 'src/app/shared/general/general-service.service';
 
 @Component({
   selector: 'app-timeline',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
-  constructor() { }
+  tweets = [];
+
+  @ViewChild('newTweet', { static: false }) newTweet: ElementRef;
+
+  constructor(private generalService: GeneralService) { }
 
   ngOnInit() {
+  }
+
+  handleNewTweet(event) {
+    if (event.keyCode !== 13) {
+      return;
+    }
+
+    const author = this.generalService.getUsername();
+    const content = this.newTweet.nativeElement.value;
+
+    const tweet = {
+      author,
+      content,
+      likes: 0
+    }
+
+    this.tweets.unshift(tweet);
+
+    this.newTweet.nativeElement.value = '';
   }
 
 }
